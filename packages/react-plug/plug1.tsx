092f3cdf3a00034b24048fff965069b098fa484b
zyking <zyking@zykingdeMacBook-Pro.local>
2018-04-23 19:09:21 +0800

    test

---
+++ packages/react-plug/plug1.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default ()=>{
+    return  <div><h1>我aqqqq</h1></div>
+}
---
+++ packages/umi-host/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Plug1 from 'react-plug/plug1'
+
+export default ()=>{
+    return  <div>
+        <h1>index.tsx</h1>
+        <Plug1 />
+        </div>
+}
